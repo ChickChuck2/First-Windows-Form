4a4804ba16bc62fe006a8b03d7418b47b47de873
ChickChuck2 <48648882+ChickChuck2@users.noreply.github.com>
2021-03-29 20:18:52 -0300

    mensagem

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,27 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.Botao1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Botao1
+            // 
+            this.Botao1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Botao1.Location = new System.Drawing.Point(372, 237);
+            this.Botao1.Name = "Botao1";
+            this.Botao1.Size = new System.Drawing.Size(128, 23);
+            this.Botao1.TabIndex = 0;
+            this.Botao1.Text = "Clica aqui por favor";
+            this.Botao1.UseVisualStyleBackColor = true;
+            this.Botao1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(842, 500);
+            this.Controls.Add(this.Botao1);
             this.Name = "Form1";
             this.Text = "Primeiro coisinha em C#";
             this.ResumeLayout(false);
@@ -43,6 +57,8 @@ namespace WindowsFormsApp1
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Botao1;
     }
 }
 
