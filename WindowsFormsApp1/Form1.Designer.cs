ae0b5082ca0c0617af24d598b9b7cdf216f1242a
ChickChuck2 <48648882+ChickChuck2@users.noreply.github.com>
2021-03-29 21:37:50 -0300

    SEXXX

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,13 +30,14 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.Botao1 = new System.Windows.Forms.Button();
+            this.texto1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Botao1
             // 
             this.Botao1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.Botao1.Location = new System.Drawing.Point(372, 237);
+            this.Botao1.Location = new System.Drawing.Point(334, 239);
             this.Botao1.Name = "Botao1";
             this.Botao1.Size = new System.Drawing.Size(128, 23);
             this.Botao1.TabIndex = 0;
@@ -44,21 +45,35 @@ namespace WindowsFormsApp1
             this.Botao1.UseVisualStyleBackColor = true;
             this.Botao1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // texto1
+            // 
+            this.texto1.AutoSize = true;
+            this.texto1.Location = new System.Drawing.Point(334, 127);
+            this.texto1.Name = "texto1";
+            this.texto1.Size = new System.Drawing.Size(88, 13);
+            this.texto1.TabIndex = 1;
+            this.texto1.Text = "Você vai Morrer!!";
+            this.texto1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(842, 500);
+            this.Controls.Add(this.texto1);
             this.Controls.Add(this.Botao1);
             this.Name = "Form1";
             this.Text = "Primeiro coisinha em C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button Botao1;
+        private System.Windows.Forms.Label texto1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +20,23 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            SoundPlayer somzao = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+
             MessageBox.Show("Botão foi clicado caralho");
+
+            somzao.Play();
+
+            texto1.Text = "Procuro sexo";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
